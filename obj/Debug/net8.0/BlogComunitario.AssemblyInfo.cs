d9b16a1555439ea66e51d3ba6e2e4592f259884d
Paulo  Castanha <paulo.castanha@kataca.local>
2024-09-24 14:46:22 +0100

    Novo

--- obj/Debug/net8.0/BlogComunitario.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlogComunitario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogComunitario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9674893ae1cb341a51edd044cc2061ec17c3454")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16321e71f86357e90f89d9f938749ae140e5bdfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogComunitario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogComunitario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
